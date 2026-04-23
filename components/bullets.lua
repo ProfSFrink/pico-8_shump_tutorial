83fc602a29fd7bb02f855a80a770e613e7186c2f
Steven Partlow <stevenpartlow@outlook.com>
2026-04-23 17:43:34 +0100

    - (refactor) separate bullet and laser behaviours.

--- components/bullets.lua
+++ components/bullets.lua
@@ -1,27 +1,35 @@
 -- Factory function for creating bullets
 	-- @param x: The x position of the bullet.
 	-- @param y: The y position of the bullet.
-	-- @param sprStart: The starting frame of the bullet's animation.
-	-- @param sprEnd: The ending frame of the bullet's animation.
-	-- @param speed: The speed of the bullet.
+	-- @param strtFram: The starting frame of the bullet's animation.
+	-- @param endFram: The ending frame of the bullet's animation.
+	-- @param spd: The speed of the bullet.
 	-- @return: A new bullet object.
-function newBullet(x, y, sprStart, sprEnd, speed)
+function newBullet(x, y, strtFram, endFram, spd)
 	return {
 		x=x,
 		y=y,
-		speed=speed,
-		currentFrame=sprStart,
-		spriteStart=sprStart,
-		spriteEnd=sprEnd,
+		spd=spd,
+		curFram=strtFram,
+		strtFram=strtFram,
+		endFram=endFram,
+		framDelay=0,
 
 		-- Update the bullet's position.
 		update=function(self)
-            self.y=self.y-self.speed
+            self.y=self.y-self.spd
 
 			-- Animate the bullet.
-            self.currentFrame=self.currentFrame+1
-            if self.currentFrame >= self.spriteEnd then
-                self.currentFrame=self.spriteStart
+            self.framDelay+=1
+            if self.framDelay>=5 then
+				self.framDelay=0
+                self.curFram+=1
+
+                if self.curFram == self.endFram then
+                    self.curFram=self.endFram
+				else
+					self.curFram=self.strtFram
+				end
             end
 
 			-- Remove the bullet if it
@@ -33,7 +41,53 @@ function newBullet(x, y, sprStart, sprEnd, speed)
 
 		-- Draw the bullet to the screen.
 		draw=function(self)
-			spr(self.currentFrame, self.x, self.y)
+			spr(self.curFram, self.x, self.y)
+		end
+	}
+end
+
+-- Factory function for creating lasers
+	-- @param x: The x position of the laser.
+	-- @param y: The y position of the laser.
+	-- @param strtFram: The starting frame of the laser's animation.
+	-- @param endFram: The ending frame of the laser's animation.
+	-- @param spd: The speed of the laser.
+	-- @return: A new laser object.
+function newLaser(x, y, strtFram, endFram, spd)
+	return {
+		x=x,
+		y=y,
+		spd=spd,
+		curFram=strtFram,
+		strtFram=strtFram,
+		endFram=endFram,
+		framDelay=0,
+
+		-- Update the laser's position.
+		update=function(self)
+            self.y=self.y-self.spd
+
+			-- Animate the laser.
+            self.framDelay+=1
+            if self.framDelay>=6 then
+				self.framDelay=0
+                self.curFram+=1
+
+                if self.curFram == self.endFram then
+                    self.curFram=self.endFram
+				end
+            end
+
+			-- Remove the laser if it
+			-- disappears behind the UI.
+			if self.y < uiHeight-bullHeight then
+				del(bullets, self)
+			end
+		end,
+
+		-- Draw the laser to the screen.
+		draw=function(self)
+			spr(self.curFram, self.x, self.y)
 		end
 	}
 end
